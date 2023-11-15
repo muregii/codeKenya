e48c9dcd584ecb4175ad09c448184d1cc7253b79
Raydon <88530358+muregii@users.noreply.github.com>
2023-11-14 23:52:39 -0500

    Time: 0 ms (100.00%), Space: 39.5 MB (12.78%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -0,0 +1,17 @@
+class Solution {
+     
+    public int getSum(int a, int b) {
+        
+        while(b != 0){
+          
+           int carry =  (a & b) << 1;
+            
+            a = a ^ b;
+            
+            b = carry;
+        }
+        return a;
+        
+    }
+    
+}
