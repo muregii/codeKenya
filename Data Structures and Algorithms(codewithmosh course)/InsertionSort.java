e9eaa2a206cecd011bddee962e61b8d97471ccf3
muregii <88530358+muregii@users.noreply.github.com>
2023-11-24 23:56:36 -0500

    Insertion Sort added by <raydonmuregi@gmail.com>

---
+++ Data Structures and Algorithms(codewithmosh course)/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+   public void sort(int[] array){
+    for(var i = 1; i < array.length; i++){
+        var current = array[i];
+        int j = i-1;
+        while(j >= 0 && array[j] > current){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = current;
+    }
+   }
+    
+}
--- Data Structures and Algorithms(codewithmosh course)/Main.java
+++ Data Structures and Algorithms(codewithmosh course)/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main{
   public static void main(String[] args) {
    int[] numbers = {8, 7 , 6, 5, 4, 3, 2, 1};
-   var sorter = new SelectionSort();
+   var sorter = new InsertionSort();
    sorter.sort(numbers);
    System.out.println(Arrays.toString(numbers));
     
