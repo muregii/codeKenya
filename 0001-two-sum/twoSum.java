8b38b3c649c1191662af6534e99ff2b4bfb7ae79
Kennedylelei <kennedylelei26@gmail.com>
2023-11-29 12:33:37 +0300

    Two sum with main func

--- 0001-two-sum/twoSum.java
+++ 0001-two-sum/twoSum.java
@@ -1,8 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static void main (String[] args) {
+        int[] val = {22,0,11,9};
+        int tgt = 9;
+        int[] result = twoSum(val, tgt);
+        System.out.println(Arrays.toString(result));
+    } 
+
+    public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> complements = new HashMap<>();
         for (int i = 0; i < nums.length; i++){
             Integer complementIndex = complements.get(nums[i]);
