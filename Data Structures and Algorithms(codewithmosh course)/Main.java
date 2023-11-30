c86a864ada367821276ecd6fe0b41d11ca5df0a9
muregii <88530358+muregii@users.noreply.github.com>
2023-11-29 19:16:30 -0500

    Day 2 of the challenge. Recap of the valid BST Leetcode question

--- Data Structures and Algorithms(codewithmosh course)/Main.java
+++ Data Structures and Algorithms(codewithmosh course)/Main.java
@@ -1,57 +1,54 @@
 public class Main {
 
     public static void main(String[] args) {
-        IsValidBST bstChecker = new IsValidBST();
+        //IsValidBST bstChecker = new IsValidBST();
+        IsValidBST test = new IsValidBST();
 
-        // Kubuild our tree. Hapa unaeza tumia
-        bstChecker.root = bstChecker.new TreeNode(10);
-        bstChecker.root.leftChild = bstChecker.new TreeNode(5);
-        bstChecker.root.rightChild = bstChecker.new TreeNode(15);
-        bstChecker.root.leftChild.leftChild = bstChecker.new TreeNode(5);
-        bstChecker.root.leftChild.rightChild = bstChecker.new TreeNode(7);
-        bstChecker.root.rightChild.rightChild = bstChecker.new TreeNode(18);
+        test.root = test.new TreeNode(10);
+        test.root.leftChild = test.new TreeNode(5);
+        test.root.rightChild = test.new TreeNode(15);
+        test.root.leftChild.leftChild = test.new TreeNode(5);
+        test.root.leftChild.rightChild = test.new TreeNode(7);
+        test.root.rightChild.rightChild = test.new TreeNode(15);
 
-        // Testing kama tree yetu is indeed a valid binary tree
-        boolean isValid = bstChecker.isValidBST();
-        System.out.println("Is the tree a valid BST? " + isValid);
+        var answer = test.isValidBST();
+        System.out.println("Is the tree a valid BST? " + answer);
+
     }
 
-    public static class IsValidBST{ //Hii static hapa ni muhimu ndio main method iweze kuload information iko kwa hii class
+    public static class IsValidBST{
 
-      private class TreeNode{
-          
-          private TreeNode leftChild;
-          private TreeNode rightChild;
-          private int info;
-          
-          public TreeNode(int info) {
-              this.info = info;
-          }
-  
-          @Override
-          public String toString(){
-              return "Node=" + info;
+        private class TreeNode{
+            private TreeNode leftChild;
+            private TreeNode rightChild;
+
+            private int info;
+
+            public TreeNode(int info) {
+                this.info = info;
+            }
+
+            @Override
+            public String toString(){
+                return "Node =" + info;
+            }
+
         }
-          
-      }
+
+        private TreeNode root;
+
+        public boolean isValidBST(){
+            return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE );
+        }
+
+        private boolean isValidBST(TreeNode root, int lefts, int rights){
+            if(root == null) return true;
+
+            if(root.info < lefts || root.info > rights) return false;
+
+            return isValidBST(root.leftChild, lefts, root.info-1) && isValidBST(root.rightChild, root.info, rights);
+        }
+    }
   
-      private TreeNode root;
-  
-      public boolean isValidBST(){
-  
-          return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-  
-      }
-  
-      private boolean isValidBST(TreeNode root, int lefts, int rights){
-          
-          if(root == null) return true;
-          
-          if(root.info < lefts || root.info > rights) return false;
-  
-          return isValidBST(root.leftChild, lefts, root.info -1) && isValidBST(root.rightChild, root.info, rights);
-  
-      }
-  }
 
 }
