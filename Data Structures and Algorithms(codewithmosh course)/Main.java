e84fdfdcbcda6d4450e29d6eabd8c7a6d5d19160
muregii <88530358+muregii@users.noreply.github.com>
2023-12-03 17:33:11 -0500

    Day 6 - LinkedList Introduction and HashTable recap. The two sum Question

--- Data Structures and Algorithms(codewithmosh course)/Main.java
+++ Data Structures and Algorithms(codewithmosh course)/Main.java
@@ -1,54 +1,33 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        //IsValidBST bstChecker = new IsValidBST();
-        IsValidBST test = new IsValidBST();
-
-        test.root = test.new TreeNode(10);
-        test.root.leftChild = test.new TreeNode(5);
-        test.root.rightChild = test.new TreeNode(15);
-        test.root.leftChild.leftChild = test.new TreeNode(5);
-        test.root.leftChild.rightChild = test.new TreeNode(7);
-        test.root.rightChild.rightChild = test.new TreeNode(15);
-
-        var answer = test.isValidBST();
-        System.out.println("Is the tree a valid BST? " + answer);
-
+        Solution sol = new Solution();
+    
+        int[] val = {22, 0, 11, 9};
+        int tgt = 9;
+        int[] result = sol.twoSum(val, tgt); // Use 'sol' to call the method
+        System.out.println(Arrays.toString(result));
     }
 
-    public static class IsValidBST{
+   public static class Solution {
+   
 
-        private class TreeNode{
-            private TreeNode leftChild;
-            private TreeNode rightChild;
-
-            private int info;
-
-            public TreeNode(int info) {
-                this.info = info;
-            }
-
-            @Override
-            public String toString(){
-                return "Node =" + info;
-            }
-
+    public static int[] twoSum(int[] nums, int target) {
+       //Key | Value  nums [] = [2,7,11,15] 
+        //                       0 1 2 3 
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])) return new int[] {map.get(target - nums[i]), i };
+        map.put(nums[i], i); //(Key)2 | (Value) 0
         }
-
-        private TreeNode root;
-
-        public boolean isValidBST(){
-            return isValidBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE );
-        }
-
-        private boolean isValidBST(TreeNode root, int lefts, int rights){
-            if(root == null) return true;
-
-            if(root.info < lefts || root.info > rights) return false;
-
-            return isValidBST(root.leftChild, lefts, root.info-1) && isValidBST(root.rightChild, root.info, rights);
-        }
+        throw new IllegalArgumentException();
     }
+
+}
   
 
 }
