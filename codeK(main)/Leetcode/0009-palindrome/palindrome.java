7e31e91fe0a463c4686b49820a48bfa426b05f11
Kennedylelei <kennedylelei26@gmail.com>
2023-12-06 00:11:11 +0300

    Debugged

--- codeK(main)/Leetcode/0009-palindrome/palindrome.java
+++ codeK(main)/Leetcode/0009-palindrome/palindrome.java
@@ -6,13 +6,13 @@ class Palindrome {
 
     public static boolean isPalindrome(int x) {
         int temp=x ,rev = 0;
-        if (x < 0) return false;
+        
         while(x > 0){
             int digit = x % 10;
             rev = rev * 10 + digit; 
             x = x / 10;
         }
-        if(temp == x){
+        if(rev == temp){
             return true;
         }else{
             return false;
