73989a4368dd32924dfb16507e25039121b93b3c
Kennedylelei <kennedylelei26@gmail.com>
2023-12-06 10:10:07 +0300

    comments

--- codeK(main)/Leetcode/0007-reverse-integer/reverseInt.java
+++ codeK(main)/Leetcode/0007-reverse-integer/reverseInt.java
@@ -3,9 +3,9 @@ public class reverseInt {
         int rev = 0;
 
         while(x != 0) {
-            int digit = x % 10;
-            rev = (rev * 10) + digit;
-            x = x / 10;
+            int digit = x % 10; //gets the last digit of the int
+            rev = (rev * 10) + digit; //reverses the last digit
+            x = x / 10; //goes to the next digit 
         }
 
         return rev;
