cfeb18ab6989e62823964dd5f0723bab286fcb35
Raydon <88530358+muregii@users.noreply.github.com>
2023-12-07 04:12:53 -0500

    Time: 4 ms (100.00%), Space: 43.1 MB (27.53%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        while(x != 0){
+            //1234
+            int digit = x % 10;
+            rev = (rev * 10) + digit;
+            x /= 10;
+        }
+        if(temp < 0) return false;
+        if(temp == rev ) {
+            return true;
+        }
+        return false;
+    }
+}
