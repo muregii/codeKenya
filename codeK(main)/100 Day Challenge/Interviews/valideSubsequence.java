1492189c3eba884953348bb025c75542afa1699a
Kennedylelei <kennedylelei26@gmail.com>
2023-12-09 23:34:36 +0300

    Added valid sebsequence

---
+++ codeK(main)/100 Day Challenge/Interviews/valideSubsequence.java
@@ -0,0 +1,35 @@
+/*
+    VALIDATE SUBSEQUENCE
+
+    Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
+    A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array.
+    For instance, the numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4], and so do the numbers [2, 4].
+    Note that a single number in an array and the array itself are both valid subsequences of the array.
+
+    Sample Input
+    array = [5, 1, 22, 25, 6, -1, 8, 10] 
+    sequence = [1, 6, -1, 10]
+    In this case return true
+*/
+
+import java.util.*;
+
+class Program {
+  public static boolean isValidSubsequence(
+    List<Integer> array, List<Integer> sequence
+  ) {
+    // Write your code here.
+     //sequence  array < array
+    if (array.size() < sequence.size()) return false;
+
+    for (int arrayNum : array){
+        Integer sequenceNum = sequence.get(0);
+      if (arrayNum == sequenceNum){
+        sequence.remove(0);
+      }
+      if(sequence.size() == 0) return true;
+  
+    }
+    return false;
+    }
+}
