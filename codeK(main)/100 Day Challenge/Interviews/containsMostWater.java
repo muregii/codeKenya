5a6b958ca80bec3768cc5a95ecfddd476dd7d2d2
Raydon <88530358+muregii@users.noreply.github.com>
2023-12-13 07:21:21 -0500

    Update containsMostWater.java

--- codeK(main)/100 Day Challenge/Interviews/containsMostWater.java
+++ codeK(main)/100 Day Challenge/Interviews/containsMostWater.java
@@ -1,3 +1,5 @@
+//Recorded video of the interview: https://duke.zoom.us/rec/share/RN6ZUwd19FnW6Kc37KygD9l0EDhk7ttvqamfw-_sArZJb-aVyqtcyWgnbgD-GxVS.-78Fvf8S_Ji3uXbq?startTime=1702154801000
+
 class Solution {
     public int maxArea(int[] height) {
         var left = 0;
