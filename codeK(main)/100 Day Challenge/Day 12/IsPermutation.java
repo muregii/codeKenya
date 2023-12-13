03cc11f93e2dd6addbf1c51e0f3b75e352bfada0
muregii <88530358+muregii@users.noreply.github.com>
2023-12-13 00:42:56 -0500

    Day 13 code challenge

--- codeK(main)/100 Day Challenge/Day 12/IsPermutation.java
+++ codeK(main)/100 Day Challenge/Day 12/IsPermutation.java
@@ -4,6 +4,10 @@
 
 
 public class IsPermutation {
+       public static void main(String[] args) {
+    //I introduced lambda expression to help us run test cases on leetcode questions we've done. Ndio tusidelete data we've typed kwa main method to create some space in the main method    
+        IsPermutation.run();
+}
     public boolean isPermutation (String s, String t){
         
        if(s.length() != t.length()) return false;
@@ -11,25 +15,26 @@ public class IsPermutation {
 
        for(int i = 0; i < s.length(); ++i){
         letterCount[s.charAt(i)]++;
+        
+        System.out.println(letterCount[s.charAt(i)]);
        }
 
        for(int i = 0; i < s.length(); ++i){
         letterCount[t.charAt(i)]--;
-        if(letterCount[t.charAt(i)] > 0) return false;
+        System.out.println(letterCount[t.charAt(i)]);
+        if(letterCount[t.charAt(i)] < 0) return false;
+        
        }
        return true; 
         
     }
 
-    public static void main(String[] args) {
-    //I introduced lambda expression to help us run test cases on leetcode questions we've done. Ndio tusidelete data we've typed kwa main method to create some space in the main method    
-    IsPermutation.run();
-}
+ 
 
 static Runnable IsPermutation = () -> {
     IsPermutation test = new IsPermutation();
-    String s = "raydon";
-    String t = "raydon";
+    String s = "rayo";
+    String t = "rayd";
     var result = test.isPermutation(s, t);
     System.out.println("Are these two strings permutations of each other? " + result);
 };
