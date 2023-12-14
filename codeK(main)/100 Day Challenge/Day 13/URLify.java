67db73fb436292e85ad5caedd05b7c44a59f0cec
muregii <88530358+muregii@users.noreply.github.com>
2023-12-14 06:20:34 -0500

    URLify

--- codeK(main)/100 Day Challenge/Day 13/URLify.java
+++ codeK(main)/100 Day Challenge/Day 13/URLify.java
@@ -1,14 +1,42 @@
 //package codeK(main).100 Day Challenge.Day 13;
 
 public class URLify {
-    public String url(String s){
-        String[] words = s.split(" ");
-         // My, best, friend
-        //char[] str = s.toCharArray(); // ['m', 'y', 'b', 'e', 's', 't', 'f', 'r', 'i', 'e', 'n', 'd']
-        for(int i = 0; i < words.length; i++){
-          
-            
+    public StringBuilder url(String s){
+        StringBuilder bld = new StringBuilder(); //Strings are immutable in Java. As in, huezi edit the original, you have to recreate a copy somewhere. 
+     
+        for(int i = 0; i < s.length(); i++){
+          if(s.charAt(i) != ' '){
+             bld.append(s.toLowerCase().charAt(i));
+          }else{
+            bld.append("%20");
+          }         
         }
-       return ""; 
+       return bld; 
+    }
+    public StringBuilder url1(String s){
+       
+         StringBuilder bld = new StringBuilder(); //Strings are immutable in Java. As in, huezi edit the original, you have to recreate a copy somewhere. 
+         int[] letters = new int[128];
+        var chars = s.toLowerCase().toCharArray();
+        for(int i =  0 ; i < chars.length;i++){
+            if(chars[i] == 32){
+                bld.append("%20");
+            }else{
+                 bld.append(chars[i]);
+            }
+           
+        }
+       
+
+        return bld;
+        
+      
+    }
+
+    public static void main(String[] args){
+        URLify test = new URLify();
+        String s = "One a month foundation";
+        StringBuilder answer =  test.url(s);
+        System.out.println("URL of the string is: " + answer);
     }
 }
