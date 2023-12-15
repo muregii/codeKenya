6e1f5184907ca398456a950de9f32977266b5e89
Kennedylelei <kennedylelei26@gmail.com>
2023-12-15 23:38:58 +0300

    Added interview question

---
+++ codeK(main)/100 Day Challenge/Interviews/validAnagram.java
@@ -0,0 +1,22 @@
+/*Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+*/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if (s.length() != t.length()) return false;
+       
+    }
+}
