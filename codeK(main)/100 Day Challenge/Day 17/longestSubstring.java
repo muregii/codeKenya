f26fe3e294d1d5803f3efbf63bb74ac1b3c20571
Kennedylelei <kennedylelei26@gmail.com>
2023-12-19 00:07:38 +0300

    debugging

--- codeK(main)/100 Day Challenge/Day 17/longestSubstring.java
+++ codeK(main)/100 Day Challenge/Day 17/longestSubstring.java
@@ -9,15 +9,15 @@ class LongestSubstring {
     }
     public static int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
-        HashMap<Character, Integer> stringMap = new HashMap<>();
+        HashMap<Character, Integer> stringMap = new HashMap<>();//new HashMap
         //s = {a,b,c,a,b,c,b,b}
-        for (int i = 0; i < s.length(); i++) {
-            if (stringMap.containsKey(s.charAt(i))) {
-                return maxLength = stringMap.get(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) { //loops through the string
+            if (stringMap.containsKey(s.charAt(i))) { //Checks if the char is already in the HashMap
+                return maxLength = i - stringMap.get(s.charAt(i)); //returns current value of i - (index of the already initial char)
             } else {
-                stringMap.put(s.charAt(i), i);
+                stringMap.put(s.charAt(i), i);//if not repeated, adds the char to the map
             }
         }
-        return maxLength;
+        return maxLength; // Returns the max length of the substring
     }
 }
