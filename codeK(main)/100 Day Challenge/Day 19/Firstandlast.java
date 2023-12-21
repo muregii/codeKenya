fd17ed9929d7b473e7784a8d2f2ac2e8b1c47529
muregii <88530358+muregii@users.noreply.github.com>
2023-12-21 13:23:17 -0500

    Created my own class in Day 19 to solve the firstandlast coding problem

---
+++ codeK(main)/100 Day Challenge/Day 19/Firstandlast.java
@@ -0,0 +1,15 @@
+/*
+ * Given a sorted array of integers arr and an integer target,
+ * find the index of the first and last position of target in arr.
+ * If target can't be found in arr, return[-1,-1]
+ * 
+ * Example input: 
+ * arr: [2,4,5,5,5,5,5,7,9,9]
+ * target = 5;
+ * output: [2,6]
+ */
+public class Firstandlast {
+    public int[] targetIndex(int[] arr, int target){
+        
+    }
+}
