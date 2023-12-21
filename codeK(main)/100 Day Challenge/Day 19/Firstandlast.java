ead26954637803d29134f6df558c3a27c2b071e3
muregii <88530358+muregii@users.noreply.github.com>
2023-12-21 15:30:57 -0500

    Solved first and last

--- codeK(main)/100 Day Challenge/Day 19/Firstandlast.java
+++ codeK(main)/100 Day Challenge/Day 19/Firstandlast.java
@@ -8,8 +8,53 @@
  * target = 5;
  * output: [2,6]
  */
+
+import java.util.*;
+
 public class Firstandlast {
-    public int[] targetIndex(int[] arr, int target){
+    // public int[] targetIndex(int[] arr, int target){
+    //     Map<Integer, Integer> map = new HashMap<>();
+       
+    //     for(int i = 0; i < arr.length; i++){
+    //         if(map.containsKey(target)){
+    //             return new int[] {map.get(target), };
+    //         }
+    //         map.put(arr[i], i);
+                 
+    //     }
+    // }
+
+    public static int[] targetIndex(int[] arr, int target){
+        int left = 0;
+        int right = arr.length-1;
+        int first = -1;
+        int last = -1;
         
+        
+        while(left <= right){
+         
+            if(arr[left] == target) {
+                first = left;
+               break;
+            }
+            left++;
+        }
+
+        while(left <= right){
+            if(arr[right] == target){
+                last = right;
+                break;
+            }
+            right--;
+        }
+            
+        return new int[]{first, last};
+
+    }
+    public static void main(String[] args){
+        
+        int[] arr = {1, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6};
+        int[] result = Firstandlast.targetIndex(arr, 2);
+        System.out.println(Arrays.toString(result));
     }
 }
