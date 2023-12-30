3b90183c71e75670564c9bb038c758d84799ce50
muregii <88530358+muregii@users.noreply.github.com>
2023-12-29 22:33:02 -0500

    Deleted unwarranted GSoC folder, check it up for yourself later, but as of now, It's not related to what we're doing here."

--- Resources/Algorithmic Problem Sets/APTS_codeKenya/AccessLevelTrial.java
+++
@@ -1,6 +0,0 @@
-public class AccessLevelTrial {
-    public String canAccess(int[] rights, int minPermission) {
-           // fill in code here
-           
-    }
- }
--- Resources/Algorithmic Problem Sets/APTS_codeKenya/LengthSort.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class LengthSort {
-    public String[] rearrange(String[] values){
-        // you write code here and replace statement below
-     Arrays.sort(values, Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()));
-        return values;
-    }
-}
