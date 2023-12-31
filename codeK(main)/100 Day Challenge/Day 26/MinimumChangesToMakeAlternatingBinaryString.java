8e2e718b1f4c62651c0ee964f35e6091fa4ce75b
muregii <88530358+muregii@users.noreply.github.com>
2023-12-31 00:15:16 -0500

    commit xchanges

--- codeK(main)/100 Day Challenge/Day 26/MinimumChangesToMakeAlternatingBinaryString.java
+++ codeK(main)/100 Day Challenge/Day 26/MinimumChangesToMakeAlternatingBinaryString.java
@@ -5,7 +5,7 @@ public class MinimumChangesToMakeAlternatingBinaryString {
     public int minOperations(String s){
       return  Math.min(count(s, '0'), count(s, '1'));
     }
-
+    
      private int count(String s, char ch){
         var count = 0;
         var currentChar = s.toCharArray();
