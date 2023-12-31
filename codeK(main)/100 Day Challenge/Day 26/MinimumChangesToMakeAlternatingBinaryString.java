beec6fbac023b9c6532e48ecef845631d08329f7
muregii <88530358+muregii@users.noreply.github.com>
2023-12-31 01:16:08 -0500

    Debugged typo errors

--- codeK(main)/100 Day Challenge/Day 26/MinimumChangesToMakeAlternatingBinaryString.java
+++ codeK(main)/100 Day Challenge/Day 26/MinimumChangesToMakeAlternatingBinaryString.java
@@ -2,28 +2,28 @@
 
 public class MinimumChangesToMakeAlternatingBinaryString {
     
-    public int minperations(String s){
-      return  Math.min(count(s, '0'), count(s, '1'));
-    }
-
-     private int count(String s, char ch){
-        var count = 0;
-        var stringArray = s.toCharArray();
-        for(int i = 0; i < stringArray.length; i++){
-            if(stringArray[i] != ch){
-                count++;
-            }
-
-            if(ch == '0'){
-                ch = '1';
-            }else{
-                ch = '0';
-            }
-
-            
-        }
-        return count;
-
+    public int minOperations(String s){
+        return  Math.min(countOps(s, '0'), countOps(s, '1'));
+      }
+  
+       private int countOps(String s, char ch){
+          var count = 0;
+          var stringArray = s.toCharArray();
+          for(int i = 0; i < stringArray.length; i++){
+              if(stringArray[i] != ch){
+                  count++;
+              }
+  
+              if(ch == '0'){
+                  ch = '1';
+              }else{
+                  ch = '0';
+              }
+  
+              
+          }
+          return count;
+  
     }
 
 }
