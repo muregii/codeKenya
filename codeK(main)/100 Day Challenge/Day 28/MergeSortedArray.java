9de737e7b6eb7913ce78fed371a5bf145bda8ce3
muregii <88530358+muregii@users.noreply.github.com>
2024-01-03 01:31:19 -0500

    mergesort Top 150 interview question 1. Leetcode #88

--- codeK(main)/100 Day Challenge/Day 28/MergeSortedArray.java
+++ codeK(main)/100 Day Challenge/Day 28/MergeSortedArray.java
@@ -1,6 +1,7 @@
 //Top 150 interview. #88
 public class MergeSortedArray {
-    public int[] merge(int[] s, int[] t){
+    public int[] merge(int[] a, int m,  int[] b, int n){
+
         return new int[]{0, 0};
     }
 }
