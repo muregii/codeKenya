0fc03fd78556fbb3bd9fba1263bb85bd9427c98d
muregii <88530358+muregii@users.noreply.github.com>
2024-01-05 05:04:29 -0500

    reverted changes to WordLadder

--- codeK(main)/Top 150 Interview Questions/0127-word-ladder/WordLadder.java
+++ codeK(main)/Top 150 Interview Questions/0127-word-ladder/WordLadder.java
@@ -1,53 +1,55 @@
 import java.util.*;
 
 public class WordLadder {
-    public String ladderExists(String[] words, String from, String to) {
-        List<String> wordList = Arrays.asList(words);
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+       
         Set<String> set = new HashSet<>(wordList);
-        if (!set.contains(to)) return "no";
+        if(!set.contains(endWord)) return 0;
 
-        int changes = 1;
+        var changes = 1;
+
         Set<String> visited = new HashSet<>();
-        visited.add(from);
+        //visited.add(beginWord);
 
         Set<String> fromTheStart = new HashSet<>();
-        fromTheStart.add(from);
-
+        fromTheStart.add(beginWord);
+       
         Set<String> fromTheEnd = new HashSet<>();
-        fromTheEnd.add(to);
+        fromTheEnd.add(endWord);
 
-        while (!fromTheStart.isEmpty() && !fromTheEnd.isEmpty()) {
-            if (fromTheStart.size() > fromTheEnd.size()) {
+        while(!fromTheStart.isEmpty() && !fromTheEnd.isEmpty()){
+            if(fromTheStart.size() > fromTheEnd.size()){
                 Set<String> temp = fromTheStart;
                 fromTheStart = fromTheEnd;
                 fromTheEnd = temp;
             }
 
             Set<String> temp = new HashSet<>();
-            for (String word : fromTheStart) {
+            for(var word : fromTheStart){
                 char[] arrChars = word.toCharArray();
-                for (int i = 0; i < arrChars.length; i++) {
-                    for (char k = 'a'; k <= 'z'; k++) {
+                for(int i = 0; i < arrChars.length; i++){
+                    for(char k = 'a'; k <= 'z'; k++){
                         char tmp = arrChars[i];
                         arrChars[i] = k;
 
                         String target = String.valueOf(arrChars);
 
-                        if (fromTheEnd.contains(target)) {
-                            return "yes";
+                        if(fromTheEnd.contains(target)){
+                            return changes + 1;
                         }
 
-                        if (!visited.contains(target) && set.contains(target)) {
+                        if(!visited.contains(target) && set.contains(target)){
                             temp.add(target);
                             visited.add(target);
                         }
                         arrChars[i] = tmp;
+
                     }
                 }
             }
             fromTheStart = temp;
             changes++;
         }
-        return "no";
+        return 0;
     }
 }
