3f830d25e4a5d0fef20154f4d9f9549b498f1d3d
muregii <88530358+muregii@users.noreply.github.com>
2024-01-05 04:06:42 -0500

    Jan 5th

--- codeK(main)/100 Day Challenge/Day 27_New Year's Day 2024/WordLadder.java
+++ codeK(main)/100 Day Challenge/Day 27_New Year's Day 2024/WordLadder.java
@@ -1,83 +1,6 @@
 import java.util.*;
-/* So, doubling down on my area assigned to specialize. Graphs💀💀💀
 
-Time spent today just kuelewa swali na kuthink about this question: 4 hrs
-
- Difficulty: Hard
-
- * Concepts: 
- * 1. HashTable
- * 2. String
- * 3. BFS - Breadth First Search.
- * 4. Bidirectional BFS
- * 
- * This thing is like 50 lines of code bruh
- */
 public class WordLadder {
-    public class Solution {
-        //Using Bidirectional BFS O(M^2 * N(1/2))
-        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Set<String> set = new HashSet<>(wordList);
-            if(!set.contains(endWord)) return 0;
-            var changes = 1;
-
-            Set<String> fromTheStart = new HashSet<>();
-            Set<String> fromTheEnd = new HashSet<>();
-            Set<String> visited = new HashSet<>();
-
-            fromTheStart.add(beginWord);
-            fromTheEnd.add(endWord);
-
-            //swap
-          
-            while(!fromTheEnd.isEmpty() && !fromTheStart.isEmpty()){
-                if(fromTheStart.size() > fromTheEnd.size()){
-                Set<String> temp = fromTheStart;
-                fromTheStart = fromTheEnd;
-                fromTheEnd = temp;
-            }
-            //find the word
-            Set<String> temp = new HashSet<>();
-
-            for(var word : fromTheStart){
-                char[] arrChars = word.toCharArray();
-
-                for(int i = 0; i < arrChars.length; i++){
-                     for(char k = 'a'; k <= 'z'; k++){
-                        var tmp = arrChars[i];
-                        arrChars[i] = k;
-
-                        String target = String.valueOf(arrChars);
-
-                        if(fromTheEnd.contains(target)) {
-                            return changes + 1;
-                        }
-
-                        if(!visited.contains(target) && set.contains(target)) {
-                            temp.add(target);
-                            visited.add(target);
-                        }
-                        arrChars[i] = tmp;
-                     }
-                }
-                
-            }
-            fromTheStart = temp;
-            changes++;
-
-            }
-           return 0;
-        }
-    }
-}
-
-
-
-
-
-//Solution 1: O(M^2* N)
-/*
- * class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
     var changes = 1;
     Set<String> set = new HashSet<>(wordList);
@@ -113,4 +36,3 @@ public class WordLadder {
 
     }
 }
- */
--- codeK(main)/Top 150 Interview Questions/0127-word-ladder/WordLadder.java
+++ codeK(main)/Top 150 Interview Questions/0127-word-ladder/WordLadder.java
@@ -1,7 +1,55 @@
 import java.util.*;
 
 public class WordLadder {
-    public int ladderlength(String beginWord, String endWord, List<String> wordList){
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+       
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(endWord)) return 0;
+
+        var changes = 1;
+
+        Set<String> visited = new HashSet<>();
+        //visited.add(beginWord);
+
+        Set<String> fromTheStart = new HashSet<>();
+        fromTheStart.add(beginWord);
+       
+        Set<String> fromTheEnd = new HashSet<>();
+        fromTheEnd.add(endWord);
+
+        while(!fromTheStart.isEmpty() && !fromTheEnd.isEmpty()){
+            if(fromTheStart.size() > fromTheEnd.size()){
+                Set<String> temp = fromTheStart;
+                fromTheStart = fromTheEnd;
+                fromTheEnd = temp;
+            }
+
+            Set<String> temp = new HashSet<>();
+            for(var word : fromTheStart){
+                char[] arrChars = word.toCharArray();
+                for(int i = 0; i < arrChars.length; i++){
+                    for(char k = 'a'; k <= 'z'; k++){
+                        char tmp = arrChars[i];
+                        arrChars[i] = k;
+
+                        String target = String.valueOf(arrChars);
+
+                        if(fromTheEnd.contains(target)){
+                            return changes + 1;
+                        }
+
+                        if(!visited.contains(target) && set.contains(target)){
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                        arrChars[i] = tmp;
+
+                    }
+                }
+            }
+            fromTheStart = temp;
+            changes++;
+        }
         return 0;
     }
 }
