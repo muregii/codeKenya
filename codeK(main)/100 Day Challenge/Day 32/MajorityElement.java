d0bee1e86fdf681ee13377a4d8d4a07654ff6914
muregii <88530358+muregii@users.noreply.github.com>
2024-01-09 00:05:12 -0500

    Trial 1. <Failed>< Runtime error>

--- codeK(main)/100 Day Challenge/Day 32/MajorityElement.java
+++ codeK(main)/100 Day Challenge/Day 32/MajorityElement.java
@@ -3,11 +3,35 @@
  * Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
- */
+Concepts:
+1. Arrays
+2. HashTable
+3. Divide and Conquer
+4.  
+
+*/
 
 
 public class MajorityElement {
     public int majorityElement(int[] nums) {
+        var size = nums.length;
+        var count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], count++);
+                
+            }
+            map.put(nums[i], count);
+            
+        }
+
+        for(var i : nums){
+            if(map.get(nums[i]) > size){
+                return nums[i];
+            }
+        }
+        
         return 0;
     }
 }
