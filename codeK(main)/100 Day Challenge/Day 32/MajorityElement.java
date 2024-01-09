e08b553e311444346881e5b831b75e5ca4d063d5
muregii <88530358+muregii@users.noreply.github.com>
2024-01-08 23:43:18 -0500

    Commit 1 <MajorityElement><raydonmuregi>

--- codeK(main)/100 Day Challenge/Day 32/MajorityElement.java
+++ codeK(main)/100 Day Challenge/Day 32/MajorityElement.java
@@ -1,4 +1,10 @@
-//package codeK(main).100 Day Challenge.Day 32;
+//Swali interesting hapa, also found in the Top 150 interview questions
+/*
+ * Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ */
+
 
 public class MajorityElement {
     
