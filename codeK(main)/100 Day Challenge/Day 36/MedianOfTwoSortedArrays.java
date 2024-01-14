e6d55a917626d52f34a892d15c7632755583194b
muregii <88530358+muregii@users.noreply.github.com>
2024-01-13 23:51:17 -0500

    Added find method

--- codeK(main)/100 Day Challenge/Day 36/MedianOfTwoSortedArrays.java
+++ codeK(main)/100 Day Challenge/Day 36/MedianOfTwoSortedArrays.java
@@ -1,5 +1,7 @@
 //package codeK(main).100 Day Challenge.Day 36;
 
 public class MedianOfTwoSortedArrays {
-    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
 }
