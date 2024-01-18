a3fe8c98c641d2708577b4004a22117da27fff61
muregii <88530358+muregii@users.noreply.github.com>
2024-01-17 23:49:48 -0500

    message

--- codeK(main)/100 Day Challenge/Day 28/MergeSortedArray.java
+++ codeK(main)/100 Day Challenge/Day 28/MergeSortedArray.java
@@ -7,7 +7,7 @@ public class MergeSortedArray {
 
         while(i >= 0 && j >= 0){
             /*Input: nums1 = [1,2,2,3,5,6], m = 3, nums2 = [2,5,6], n = 3
-Output: [1,2,2,3,5,6] */
+        Output: [1,2,2,3,5,6] */
             if(a[i] > b[j]){
                 a[last] = a[i];
                 i--;
