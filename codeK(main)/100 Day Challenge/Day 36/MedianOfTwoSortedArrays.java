54b1cd7654a3412859cd04401559e2ede0b9e8c2
muregii <88530358+muregii@users.noreply.github.com>
2024-01-17 23:43:13 -0500

    Added some swahili comments

--- codeK(main)/100 Day Challenge/Day 36/MedianOfTwoSortedArrays.java
+++ codeK(main)/100 Day Challenge/Day 36/MedianOfTwoSortedArrays.java
@@ -1,5 +1,7 @@
 //Difficulty Hard
-/* */
+/* 
+ * Ukipewa array mbili, inafaa urudishe median of the sorted of combination of the two.
+*/
 public class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
