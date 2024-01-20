b9178180251caec833496291dc926e8198dc76c6
muregii <88530358+muregii@users.noreply.github.com>
2024-01-19 23:50:07 -0500

    Test 1 for remove duplicates

--- codeK(main)/100 Day Challenge/Day 31/RemoveDuplicatesFromSortedArray.java
+++ codeK(main)/100 Day Challenge/Day 31/RemoveDuplicatesFromSortedArray.java
@@ -1,8 +1,17 @@
-
+//             Input: nums = [1,1,1,2,2,3]
+//             Output: 5, nums = [1,1,2,2,3,_]
 
 public class RemoveDuplicatesFromSortedArray {
     public int removeElements(int[] nums, int val){
-        int k = 0;
-        for(int i = 0; i < )
+        if(nums.length <= 2) return nums.length;
+        int j = 2;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != nums[j - 2]){
+                nums[j] = nums[i];
+                j++;
+            }
+
+        }
+        return j;
     }
 }
