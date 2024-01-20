598c12ea5e9de795fd2cbebf621d046cf2b9dce5
muregii <88530358+muregii@users.noreply.github.com>
2024-01-20 00:00:22 -0500

    Debugged and tested

--- codeK(main)/100 Day Challenge/Day 31/RemoveDuplicatesFromSortedArray.java
+++ codeK(main)/100 Day Challenge/Day 31/RemoveDuplicatesFromSortedArray.java
@@ -4,8 +4,9 @@
 public class RemoveDuplicatesFromSortedArray {
     public int removeElements(int[] nums, int val){
         if(nums.length <= 2) return nums.length;
+
         int j = 2;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 2; i < nums.length; i++){
             if(nums[i] != nums[j - 2]){
                 nums[j] = nums[i];
                 j++;
@@ -15,3 +16,9 @@ public class RemoveDuplicatesFromSortedArray {
         return j;
     }
 }
+
+/*Hints
+ * Trick ni kustart from index 2 for both i and j
+ * kama nums[i] ni different na j two steps back, start of the array, tunacopy sasa nums[j] into nums[i]. Then of course the increments zinawork on the data, (i++ na j++)
+ * j  ndio length tutareturn pia
+ */
