7852a3a7f14182ab3e10514c432023db98bce815
muregii <88530358+muregii@users.noreply.github.com>
2024-01-26 23:56:03 -0500

    Started Implemetation of the randomizedset class()

--- codeK(main)/100 Day Challenge/Day 45/InsertDeleteGetRandom.java
+++ codeK(main)/100 Day Challenge/Day 45/InsertDeleteGetRandom.java
@@ -1,5 +1,19 @@
 
 
 public class InsertDeleteGetRandom {
+    public RandomizedSet() {
+        
+    }
     
+    public boolean insert(int val) {
+        
+    }
+    
+    public boolean remove(int val) {
+        
+    }
+    
+    public int getRandom() {
+        
+    }
 }
