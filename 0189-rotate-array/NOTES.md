414ac9c08f2ef5ae982f5a339d9c1c89daada4cb
muregii <88530358+muregii@users.noreply.github.com>
2024-02-07 00:11:26 -0500

    Added rotate array

--- 0189-rotate-array/NOTES.md
+++ 0189-rotate-array/NOTES.md
@@ -1,1 +1,26 @@
-​
+​## Rotate Array
+
+Given an integer array `nums`, rotate the array to the right by `k` steps, where `k` is non-negative.
+
+**Difficulty:** Medium
+
+### Test Cases:
+
+- **Input:** `nums = [1,2,3,4,5,6,7]`, `k = 3`  
+  **Output:** `[5,6,7,1,2,3,4]`  
+  **Explanation:**  
+  - Rotate 1 steps to the right: `[7,1,2,3,4,5,6]`
+  - Rotate 2 steps to the right: `[6,7,1,2,3,4,5]`
+  - Rotate 3 steps to the right: `[5,6,7,1,2,3,4]`
+
+- **Input:** `nums = [-1,-100,3,99]`, `k = 2`  
+  **Output:** `[3,99,-1,-100]`  
+  **Explanation:**  
+  - Rotate 1 steps to the right: `[99,-1,-100,3]`
+  - Rotate 2 steps to the right: `[3,99,-1,-100]`
+
+### Concepts:
+
+1. Array
+2. Math
+3. Two Pointers
--- codeK(main)/100 Day Challenge/Day 33/RotateArray.java
+++ codeK(main)/100 Day Challenge/Day 33/RotateArray.java
@@ -1,32 +1,32 @@
-/*Given an integer array nums, 
-rotate the array to the right by k steps, where k is non-negative 
 
-DIFFICULTY: Medium 
-TEST CASES:
-Input: nums = [1,2,3,4,5,6,7], k = 3
-Output: [5,6,7,1,2,3,4]
-Explanation:
-rotate 1 steps to the right: [7,1,2,3,4,5,6]
-rotate 2 steps to the right: [6,7,1,2,3,4,5]
-rotate 3 steps to the right: [5,6,7,1,2,3,4]
-
-
-Input: nums = [-1,-100,3,99], k = 2
-Output: [3,99,-1,-100]
-Explanation: 
-rotate 1 steps to the right: [99,-1,-100,3]
-rotate 2 steps to the right: [3,99,-1,-100]
-
-CONCEPTS:
-1. Array
-2. Math
-3. Two Pointers
-
-
-*/
 
 public class RotateArray {
-    public void rotate(int[] nums, int k) {
+    
+        public void rotate(int[] nums, int k) {
+        /* REVERSE METHOD
+            4. Handle case where k > array length
+            1.Reverse the whole array
+            2.Reverse the first k elements
+            3.Reverse the remaining n-k elements, where n is the length of the array.
+          
+            */
+            k = k % nums.length;
+            reverse(nums, 0, nums.length-1);
+            reverse(nums, 0, k-1);
+            reverse(nums, k, nums.length -1);
+        }
+    
+        private void reverse(int[] nums, int beginning, int end){
+            while(beginning < end){
+                int tempHold = nums[beginning];
+                nums[beginning] = nums[end];
+                nums[end] = tempHold;
+    
+                beginning++;
+                end--;
+            }
         
+    
+    
     }
 }
