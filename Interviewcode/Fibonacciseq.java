0425f7717e06fc70a823ec8d025f9984a8eaa56a
kavisu <kavisudaniel@gmail.com>
2024-03-13 06:19:48 +0300

    Simple fibonacci sequence using recursion

---
+++ Interviewcode/Fibonacciseq.java
@@ -0,0 +1,19 @@
+public class FibonacciSeq {
+
+    /**
+     * Calculates the nth term in the Fibonacci sequence using recursion.
+     *
+     * return The nth term in the Fibonacci sequence.
+     */
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            // Base cases: The first two terms are 0 and 1
+            return n;
+        } else {
+            // Recursive case: F(n) = F(n-1) + F(n-2)
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+
+}
