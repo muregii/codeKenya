dad36abebb6528dc347868bde454663dcbc3bee2
Student <student@Students-Laptop-2.local>
2025-06-02 10:32:30 +0300

    is balanced parentheses

---
+++ codeK(main)/Neetcode/Blind 75/balancedParentheses.py
@@ -0,0 +1,15 @@
+
+def balancedParentheses(s: str) -> bool:
+    stack = []
+
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if not stack:
+                return False
+            stack.pop()
+        
+    return len(stack) == 0
+
+print(balancedParentheses("bloomberg()"))
