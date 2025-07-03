40e358c7eada6ff3de45292441ee4a7a86da51a1
DevOmondi <collinsomondi778@gmail.com>
2025-07-03 13:57:03 +0300

    fix: add netlify toml file

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,12 +1,3 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     '@tailwindcss/postcss': {},
-//   },
-// };
-
-// export default config;
-
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
