7934f3de240544e3b4f3ca7f5f1581a11001ef94
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-05 03:17:42 +0300

    Day 1: Added FizzBuzz solution and LeetCode screenshot

---
+++ CodeKenyaS2/Day 1/tony-muriuki/fizz_buzz.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+# Example 2:
+
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+# Example 3:
+
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+# Constraints:
+
+# 1 <= n <= 104
+
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    output = solution.fizzBuzz(15)
+    print(output)
