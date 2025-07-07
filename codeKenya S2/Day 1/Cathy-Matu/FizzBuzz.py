fd53069720ec3d5f7ceb476e70df1cf9efea6f89
Cathy Matu <157466395+Cathy-matu@users.noreply.github.com>
2025-07-07 13:08:56 +0300

    FizzBuzz Solution

---
+++ codeKenya S2/Day 1/Cathy-Matu/FizzBuzz.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        result = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
