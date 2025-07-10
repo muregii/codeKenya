307e6506083c948cb2da4f520df63f972b8f8a78
Student <student@Students-Laptop-2.local>
2025-07-10 12:57:55 +0300

    Valid Anagram commentary

--- codeKenya S2/Day 4/Valid_Anagram.py
+++ codeKenya S2/Day 4/Valid_Anagram.py
@@ -1,18 +1,18 @@
 from collections import Counter
 
 class Solution:
-
+    #1. Time O(n log n)    2. Space O(n)
     def isAnagram3(self, s: str, t: str):
         return sorted(s) == sorted(t)
-    
+    #1. Time O(n)    2. Space O(k)
     def isAnagram2(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
-    
+    #1. Time O(n)    2. Space O(1) -> MOST OPTIMAL
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        counts= [0] * 26 # 1. Time O(n) 2. Space O(1)
+        counts= [0] * 26 # Space O(1)
         base = ord('a')
 
         for char in s:
